f6eca8e9854b7207920733c7cf0012e58e85835b
Darek Charlet <42811036+17DCharlet@users.noreply.github.com>
2020-03-02 20:16:27 -0600

    made buttons and intents for each activity

--- app/src/main/java/edu/nwmissouri/midtermproject/MainActivity.java
+++ app/src/main/java/edu/nwmissouri/midtermproject/MainActivity.java
@@ -2,7 +2,9 @@ package edu.nwmissouri.midtermproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //replace the class name with whatever you used!
+    public void sudokuAction(View v){
+        Intent goToSudoku = new Intent(this, Sudoku.class);
+        startActivity(goToSudoku);
+    }
+
+    public void matchMakerAction(View v){
+        Intent goToMatchMaker = new Intent(this, MatchMaker.class);
+        startActivity(goToMatchMaker);
+    }
+
+    public void twentyFortyEightAction(View v){
+        Intent goToTwentyFourtyEight = new Intent(this, TwentyFortyEight.class);
+        startActivity(goToTwentyFourtyEight);
+    }
+
+    public void mineSweeperAction(View v){
+        Intent goToMineSweeper = new Intent(this, MineSweeper.class);
+        startActivity(goToMineSweeper);
     }
 }
