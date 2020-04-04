f5db92ef670215fd7e1d8ca6d416fe1635b21fa8
MistyATG <norane247@gmail.com>
2020-04-03 20:23:57 -0700

    settoing fragments

--- app/src/main/java/edu/nwmissouri/midtermproject/SudokuGame.java
+++ app/src/main/java/edu/nwmissouri/midtermproject/SudokuGame.java
@@ -19,7 +19,13 @@ public class SudokuGame extends AppCompatActivity implements sudoku_fragments.On
         setContentView(R.layout.sudoku_fragments);
         gameBoard = new SudokuBoard();
 
-
+        int groupFragments[] = new int[]{R.id.cellGroupFragment, R.id.cellGroupFragment2, R.id.cellGroupFragment3, R.id.cellGroupFragment4,
+                R.id.cellGroupFragment5, R.id.cellGroupFragment6, R.id.cellGroupFragment7, R.id.cellGroupFragment8, R.id.cellGroupFragment9};
+        for (int i = 1; i < 10; i++) {
+            sudoku_fragments groupFragment = (sudoku_fragments) getSupportFragmentManager().findFragmentById(groupFragments[i-1]);
+            groupFragment.setID(i);
+        }
     }
+
 
 }
