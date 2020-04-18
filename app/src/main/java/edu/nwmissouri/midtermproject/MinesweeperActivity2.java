b2232e34a4b2f03d6b727d2800c6a762755e2a50
Rajeev Chapagain <31379467+Rajeevbro@users.noreply.github.com>
2020-04-17 19:32:30 -0500

    changed in layout and setups

---
+++ app/src/main/java/edu/nwmissouri/midtermproject/MinesweeperActivity2.java
@@ -0,0 +1,14 @@
+package edu.nwmissouri.midtermproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MinesweeperActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minesweeper2);
+    }
+}
