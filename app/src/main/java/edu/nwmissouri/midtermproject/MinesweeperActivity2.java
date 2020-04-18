fa1fb00f260bcd1c7144b07a3a9bd9af86e802a6
Rajeev Chapagain <31379467+Rajeevbro@users.noreply.github.com>
2020-04-17 19:38:48 -0500

    Intent fixed

--- app/src/main/java/edu/nwmissouri/midtermproject/MinesweeperActivity2.java
+++ app/src/main/java/edu/nwmissouri/midtermproject/MinesweeperActivity2.java
@@ -2,13 +2,49 @@ package edu.nwmissouri.midtermproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MinesweeperActivity2 extends AppCompatActivity {
-
+int counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minesweeper2);
+        final Intent int2=new Intent(this,MineSweeper.class);
+        Button btn2=(Button)findViewById(R.id.button102);
+        Button letsplay=(Button)findViewById(R.id.play);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(int2);
+
+            }
+        });
+
+
+
+        letsplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final TextView time=(TextView)findViewById(R.id.TIME);
+
+                new CountDownTimer(2250000,1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        time.setText(String.valueOf("TIME:"+counter));
+                        counter++;
+                    }
+                    @Override
+                    public void onFinish() {
+                        time.setText("Finished");
+                    }
+                }.start();
+            }
+        });
     }
 }
